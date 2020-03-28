d6b63090c001cf96b8f18f0d1f5bbc4871266a96
Sai Deekshith <msaideekshith@gmail.com>
2020-03-28 18:45:11 +0530

    Generic Queue

---
+++ GenericQueue/GenericQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericQueue
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> queueList = new Queue<string>();
+
+            queueList.Enqueue("AAA");
+            queueList.Enqueue("BBB");
+            queueList.Enqueue("CCC");
+
+            Console.WriteLine("At the top " + queueList.Peek());
+            
+            foreach (var item in queueList)
+            {
+                Console.WriteLine(item);
+            }
+
+            queueList.Dequeue();
+            queueList.Dequeue();
+
+            Console.WriteLine("At the top " + queueList.Peek());
+           
+            foreach (var item in queueList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
